1e742d97e51e4a5b435878a7692eb23602992e07
Shelby Neubeck <neubeckshelby@gmail.com>
2018-10-02 21:39:59 -0500

    Submitting Exercise 12

--- Exercise12/JS-Assignment/scripts/main.js
+++ Exercise12/JS-Assignment/scripts/main.js
@@ -5,32 +5,46 @@ function update() {
 	
     //Create new array.
 
-    //If first name is empty. Add it to the array
+    let messages = [];
 
+    //If first name is empty. Add it to the array
+    if (!fname) {
+        messages.push("First Name is required");
+    }
 
     //If last name is empty. Add it to the array
-
+    if (!lname) {
+        messages.push("Last  Name is required");
+    }
 
     //Email is empty. Add it to the array
-
+    if (!email) {
+        messages.push("Email is required");
+    }
 
     //Loop through array
-
-
+    let error="";
+    for (let i = 0; i < messages.length; i++) {
+        error+=messages[i] + "</br>";
+    }
+    document.getElementById("error-list").innerHTML = error;
 
     //Create javascript object for the user
+    let user = {fname:"shelby", lname:"neubeck", email:"email@email.com"};
 
-
+    let fullName =  "fname" + " " + "lname";
 
     //log the user object to the console
-
+    console.log(user);
 
     //Check to see if everything is valid
 
     //Create string for today's date. Remember, if you hard code this, it will be invalid tomorrow.
+    let submittedDate = new Date().toDateString();
+
 
     //Make the alert box.
-
+    alert("submitted on" + " " + submittedDate);
 
     /* ===================
         Your code ends here
